05f3b3a0f55032f5fb4c8b0cff375514fe692da1
unknown <zyddnys>
2019-08-08 08:28:49 +0800

    Changed database init procedure for local development

--- db/__init__.py
+++ db/__init__.py
@@ -2,15 +2,18 @@
 from init import app
 import os
 
-#from flask_pymongo import PyMongo
-#app.config["MONGO_URI"] = os.getenv('MONGODB_URL', "mongodb://db:27017/patchyvideo")
 from pymongo import MongoClient
-client = MongoClient(os.getenv('MONGODB_URL', "mongodb://db:27017/"), replicaSet = 'rs1')
-#db = PyMongo(app).db.patchyvideo
+# repilcate set is only used in production
+if os.getenv("FLASK_ENV", "development") == "production" :
+    client = MongoClient(os.getenv('MONGODB_URL', "mongodb://db:27017/"), replicaSet = 'rs1')
+else :
+    client = MongoClient(os.getenv('MONGODB_URL', "mongodb://db:27017/"))
 db = client['patchyvideo']
 
 from .TagDB import TagDB
 tagdb = TagDB(db)
+
+# add initial tag categories
 tagdb.add_category('General')
 tagdb.add_category('Character')
 tagdb.add_category('Copyright')
