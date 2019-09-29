9471b84488d1471eb10773a86268d472bd5958d9
suwadaimyojin <248975844@qq.com>
2019-09-29 22:18:29 +0800

    提交run_website.bat & run_worker_local.bat文件

---
+++ run_website.bat
@@ -0,0 +1,7 @@
+set FLASK_ENV=development
+set REDISTOGO_URL_WORKER=redis://localhost:6379
+set IMAGE_PATH=J:\pvdata2
+set MONGODB_URL=mongodb://localhost:27017/patchyvideo
+set REDISTOGO_URL=localhost
+set ENABLE_TRANSACTION=false
+python main.py
