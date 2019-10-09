3ce04a2793c2cd17ab3345500318fd06e7bb7889
zyddnys <zyddnys@outlook.com>
2019-10-09 04:51:30 -0400

    removed WSL settings

--- run_worker_local.sh
+++ run_worker_local.sh
@@ -1,7 +1,7 @@
 export FLASK_ENV=development
-export REDISTOGO_URL_WORKER=redis://192.168.0.50:6379
+export REDISTOGO_URL_WORKER=redis://127.0.0.1:6379
 export IMAGE_PATH=../images
-export MONGODB_URL=mongodb://192.168.0.50:27017/patchyvideo
-export REDISTOGO_URL=192.168.0.50
+export MONGODB_URL=mongodb://127.0.0.1:27017/patchyvideo
+export REDISTOGO_URL=127.0.0.1
 export ENABLE_TRANSACTION=false
 python3.7 worker.py
