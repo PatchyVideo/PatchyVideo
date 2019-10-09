87c24fec1f558ca73edafd8f82c2511d97d21838
zyddnys <zyddnys@outlook.com>
2019-10-08 20:14:29 -0400

    changed run config to WSL

--- run_worker_local.sh
+++ run_worker_local.sh
@@ -1,7 +1,7 @@
 export FLASK_ENV=development
-export REDISTOGO_URL_WORKER=redis://localhost:6379
-export IMAGE_PATH=pvdata2
-export MONGODB_URL=mongodb://localhost:27017/patchyvideo
-export REDISTOGO_URL=localhost
+export REDISTOGO_URL_WORKER=redis://192.168.0.50:6379
+export IMAGE_PATH=../images
+export MONGODB_URL=mongodb://192.168.0.50:27017/patchyvideo
+export REDISTOGO_URL=192.168.0.50
 export ENABLE_TRANSACTION=false
-python worker.py
+python3.7 worker.py
