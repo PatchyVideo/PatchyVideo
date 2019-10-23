655dde1c7642e9e0c460b93751b48586238362bc
zyddnys <zyddnys@outlook.com>
2019-10-23 19:18:31 -0400

    tag edit changed for single video

--- static/js/singlevideo.js
+++ static/js/singlevideo.js
@@ -5,46 +5,7 @@ $(document).ready(function() {
     $("#search-bar-query").val(query_str);
     $("#edit-current-tags").css("display", "inline");
     $("#edit-current-tags").attr("href", "javascript:showModal();");
-    tags = $("#tags").attr("content");
-    edit_str = "";
-    
-    tags.split(/ /).forEach(element => {
-        edit_str += element + "\n";
-    });
-
-    $("#edit-tags-area").text(edit_str);
-
-    $(".close").click(function(){
-        hideModal();
-    });
-    $(".ok").click(function(){
-        doEdit();
-    });
 });
-
-function doEdit() {
-    tags = $("#edit-tags-area").val().split(/\r?\n/).filter(function(i){return i})
-    $(".ok").text("Please wait");
-    postJSON("/videos/edittags.do",
-    {
-        "video_id": $("#video-id").attr("content"),
-        "tags": tags,
-    }, function(result){
-        hideModal();
-        location.reload();
-    }, function(result){
-        alert(result.data);
-        $(".ok").text("Confirm");
-    });
-}
-
-function showModal() {
-    $(".modal").css("display", "block");
-}
-
-function hideModal() {
-    $(".modal").css("display", "none");
-}
 
 function breaklink() {
     postJSON("/videos/breaklink.do",
