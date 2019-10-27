2673780f27cf1ff82678fcd2a272ac384aa67bb1
zyddnys <zyddnys@outlook.com>
2019-10-27 10:05:00 -0400

    playlist style and code style changed

--- popularity_tracker.py
+++ popularity_tracker.py
@@ -15,77 +15,77 @@ update_lock = threading.Lock()
 hitmap_update_lock = threading.Lock()
 
 class PopularityTracker(object) :
-    def __init__(self, num_bins) :
-        self.num_bins = num_bins
-        self.idx = 0
-        self.bins = [None] * (self.num_bins + 2)
-        self.hitmap = {}
-        self.hitmap_sorted = {}
-        self._try_restore()
+	def __init__(self, num_bins) :
+		self.num_bins = num_bins
+		self.idx = 0
+		self.bins = [None] * (self.num_bins + 2)
+		self.hitmap = {}
+		self.hitmap_sorted = {}
+		self._try_restore()
 
-    def _try_restore(self, session = None) :
-        try :
-            item = db.popular_tags.find_one({}, session = session)
-            self.hitmap = dict.fromkeys(item['hitmap'], 0)
-            self.hitmap_sorted = self.hitmap
-            print('Loaded tags')
-            print(self.hitmap)
-        except :
-            pass
+	def _try_restore(self, session = None) :
+		try :
+			item = db.popular_tags.find_one({}, session = session)
+			self.hitmap = dict.fromkeys(item['hitmap'], 0)
+			self.hitmap_sorted = self.hitmap
+			print('Loaded tags')
+			print(self.hitmap)
+		except :
+			pass
 
-    def _try_save(self, session = None) :
-        try :
-            item = db.popular_tags.find_one({}, session = session)
-            if item is not None :
-                db.popular_tags.delete_one({'_id': ObjectId(item['_id'])})
-            hitmap_update_lock.acquire()
-            keys = list(self.hitmap_sorted.keys())
-            hitmap_update_lock.release()
-            db.popular_tags.insert_one({'hitmap': keys}, session = session)
-        except :
-            pass
+	def _try_save(self, session = None) :
+		try :
+			item = db.popular_tags.find_one({}, session = session)
+			if item is not None :
+				db.popular_tags.delete_one({'_id': ObjectId(item['_id'])})
+			hitmap_update_lock.acquire()
+			keys = list(self.hitmap_sorted.keys())
+			hitmap_update_lock.release()
+			db.popular_tags.insert_one({'hitmap': keys}, session = session)
+		except :
+			pass
 
-    def update_current_bin(self, hitmap) :
-        current_hitmap = self.bins[self.idx]
-        if current_hitmap is not None :
-            A = Counter(current_hitmap)
-            B = Counter(hitmap)
-            self.bins[self.idx] = dict(A + B)
-        else :
-            self.bins[self.idx] = hitmap
+	def update_current_bin(self, hitmap) :
+		current_hitmap = self.bins[self.idx]
+		if current_hitmap is not None :
+			A = Counter(current_hitmap)
+			B = Counter(hitmap)
+			self.bins[self.idx] = dict(A + B)
+		else :
+			self.bins[self.idx] = hitmap
 
-    def _sort(self) :
-        hitmap_update_lock.acquire()
-        self.hitmap_sorted = dict(sorted(self.hitmap.items(), key = operator.itemgetter(1), reverse = True))
-        hitmap_update_lock.release()
+	def _sort(self) :
+		hitmap_update_lock.acquire()
+		self.hitmap_sorted = dict(sorted(self.hitmap.items(), key = operator.itemgetter(1), reverse = True))
+		hitmap_update_lock.release()
 
-    def update_popularity_and_move_to_next_bin(self) :
-        next_bin_idx = (self.idx + 1) % (self.num_bins + 2)
-        to_subtract_bin_idx = (self.idx + 2) % (self.num_bins + 2)
-        all_hitmap = Counter(self.hitmap)
-        current_bin = self.bins[self.idx] or {}
-        #print('current')
-        #print(current_bin)
-        current_bin_tags = list(current_bin.keys())
-        current_bin_tags = tagdb.filter_tags(current_bin_tags)
-        current_bin = {tag: current_bin[tag] for tag in current_bin_tags}
-        current_hitmap = Counter(current_bin)
-        all_hitmap = all_hitmap + current_hitmap
-        if self.bins[to_subtract_bin_idx] is not None :
-            #print('subtracting')
-            #print(self.bins[to_subtract_bin_idx])
-            to_subtract_hitmap = Counter(self.bins[to_subtract_bin_idx])
-            all_hitmap = all_hitmap - to_subtract_hitmap
-            self.bins[to_subtract_bin_idx] = {}
-        #print('all_hitmap')
-        #print(all_hitmap)
-        self.hitmap = all_hitmap
-        self.idx = next_bin_idx
-        self._sort()
-        with MongoTransaction(dbclient) as s :
-            self._try_save(s())
-            s.mark_succeed()
-        return self.hitmap_sorted
+	def update_popularity_and_move_to_next_bin(self) :
+		next_bin_idx = (self.idx + 1) % (self.num_bins + 2)
+		to_subtract_bin_idx = (self.idx + 2) % (self.num_bins + 2)
+		all_hitmap = Counter(self.hitmap)
+		current_bin = self.bins[self.idx] or {}
+		#print('current')
+		#print(current_bin)
+		current_bin_tags = list(current_bin.keys())
+		current_bin_tags = tagdb.filter_tags(current_bin_tags)
+		current_bin = {tag: current_bin[tag] for tag in current_bin_tags}
+		current_hitmap = Counter(current_bin)
+		all_hitmap = all_hitmap + current_hitmap
+		if self.bins[to_subtract_bin_idx] is not None :
+			#print('subtracting')
+			#print(self.bins[to_subtract_bin_idx])
+			to_subtract_hitmap = Counter(self.bins[to_subtract_bin_idx])
+			all_hitmap = all_hitmap - to_subtract_hitmap
+			self.bins[to_subtract_bin_idx] = {}
+		#print('all_hitmap')
+		#print(all_hitmap)
+		self.hitmap = all_hitmap
+		self.idx = next_bin_idx
+		self._sort()
+		with MongoTransaction(dbclient) as s :
+			self._try_save(s())
+			s.mark_succeed()
+		return self.hitmap_sorted
 
 tracker = PopularityTracker(7 * 24 * 6)
 
@@ -95,29 +95,29 @@ import json
 @basePage
 @jsonRequest
 def hit_page(rd, data) :
-    #print('hit', data.hitmap)
-    update_lock.acquire()
-    try:
-        tracker.update_current_bin(data.hitmap)
-    except :
-        pass
-    update_lock.release()
-    return "data", "your hit:" + json.dumps(data.hitmap)
+	#print('hit', data.hitmap)
+	update_lock.acquire()
+	try:
+		tracker.update_current_bin(data.hitmap)
+	except :
+		pass
+	update_lock.release()
+	return "data", "your hit:" + json.dumps(data.hitmap)
 
 @app.route("/get")
 @basePage
 def get_page(rd) :
-    try :
-        count = int(request.values['count'])
-    except :
-        count = 20
-    hitmap_update_lock.acquire()
-    #print('get')
-    #print(tracker.hitmap_sorted)
-    count = min(count, len(tracker.hitmap_sorted))
-    hitmap = list(tracker.hitmap_sorted.keys())[:count]
-    hitmap_update_lock.release()
-    return "json", {"tags": hitmap}
+	try :
+		count = int(request.values['count'])
+	except :
+		count = 20
+	hitmap_update_lock.acquire()
+	#print('get')
+	#print(tracker.hitmap_sorted)
+	count = min(count, len(tracker.hitmap_sorted))
+	hitmap = list(tracker.hitmap_sorted.keys())[:count]
+	hitmap_update_lock.release()
+	return "json", {"tags": hitmap}
 
 from apscheduler.schedulers.background import BackgroundScheduler
 
@@ -126,13 +126,13 @@ scheduler = BackgroundScheduler(daemon = True)
 scheduler.start()
 
 def update_popularity() :
-    #print('update')
-    tracker.update_popularity_and_move_to_next_bin()
+	#print('update')
+	tracker.update_popularity_and_move_to_next_bin()
 
 atexit.register(lambda: scheduler.shutdown(wait = False))
 
 if __name__ == "__main__":
-    scheduler.add_job(update_popularity, 'interval', minutes = 10)
-    #print('started')
-    app.run(host = '0.0.0.0', port = 5001)
+	scheduler.add_job(update_popularity, 'interval', minutes = 10)
+	#print('started')
+	app.run(host = '0.0.0.0', port = 5001)
 
