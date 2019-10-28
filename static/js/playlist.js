7a5ac46d9cd389df82aa36969bcb72dcb1c75368
zyddnys <zyddnys@outlook.com>
2019-10-27 21:45:59 -0400

    playlist bug fix

--- static/js/playlist.js
+++ static/js/playlist.js
@@ -55,7 +55,7 @@ function buildPageSelector(selected_page, page_count) {
 function gotoPage(page) {
     query = $("#query").attr("content");
     form = $(`<form style="display: none;" action="${window.location.href}" method="POST"><input style="display: none;" name="query" type="text" value="${query}" /><input name="page" type="text" value="${page}" /></form>`);
-    $("#content-outer").append(form);
+    $("#top-navbar").append(form);
     form.submit();
 }
 
