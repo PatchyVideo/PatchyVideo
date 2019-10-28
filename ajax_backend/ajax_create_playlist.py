5e88c0b74e7360c89ba958619a5ab1f561a7202a
zyddnys <zyddnys@outlook.com>
2019-10-27 21:21:48 -0400

    style bug fix

--- ajax_backend/ajax_create_playlist.py
+++ ajax_backend/ajax_create_playlist.py
@@ -33,3 +33,24 @@ def ajax_lists_new_do(rd, user, data):
         "pid": pid
     })
     return "json", ret
+
+@app.route('/lists/new_with_video.do', methods = ['POST'])
+@loginRequiredJSON
+@jsonRequest
+def ajax_lists_new_with_video_do(rd, user, data):
+    if len(data.title) > 128 or len(data.desc) > 2048 or len(data.cover) > 32 :
+        return "json", makeResponseFailed("Data too large(max 128 chars for title, 2048 for description)")
+    if not data.title or not data.desc :
+        return "json", makeResponseFailed("Please fill all items")
+    if data.pid :
+        result = updatePlaylistInfo(data.pid, "english", data.title, data.desc, data.cover, user)
+        if result == 'SUCCEED' :
+            pid = data.pid
+        else :
+            return "json", result
+    else :
+        pid = createPlaylist("english", data.title, data.desc, data.cover, user)
+    ret = makeResponseSuccess({
+        "pid": pid
+    })
+    return "json", ret
