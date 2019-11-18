29c5fd7c35603198712f88198134a777daf595b9
zyddnys <zyddnys@outlook.com>
2019-11-18 09:44:22 -0500

    launch updated

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ import services
 import ajax_backend
 import pages
 
-
-if __name__ == '__main__':
+if __name__ == '__main__' or __name__ == 'main' :
     from db import tagdb
     tagdb.init_autocomplete()
     app.run()
