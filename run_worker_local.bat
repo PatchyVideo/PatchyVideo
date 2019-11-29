312382b8a8653cab0d71b27f1cc75b4c380d2013
freesia <1510033076@qq.com>
2019-11-29 17:57:53 +0800

    本地部署的设置完成

--- run_worker_local.bat
+++ run_worker_local.bat
@@ -1,7 +1,8 @@
 set FLASK_ENV=development
-set REDISTOGO_URL_WORKER=redis://localhost:6379
-set IMAGE_PATH=J:\pvdata2
-set MONGODB_URL=mongodb://localhost:27017/patchyvideo
-set REDISTOGO_URL=localhost
+set REDISTOGO_URL_WORKER=redis://192.168.99.100:6379
+set IMAGE_PATH=E:\pvdata2
+set MONGODB_URL=mongodb://192.168.99.100:27017/patchyvideo
+set REDISTOGO_URL=192.168.99.100
 set ENABLE_TRANSACTION=false
 python scraper.py
+pause
