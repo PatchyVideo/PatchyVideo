e0cf3289c44c67b11dc05b0877faade926011319
zyddnys <zyddnys@outlook.com>
2019-11-29 23:25:30 -0500

    remove useless debug info

--- main.py
+++ main.py
@@ -10,8 +10,6 @@ import backend
 
 if __name__ == '__main__' or __name__ == 'main' :
     from db import tagdb
-    import sys
-    print('!!!!!!!!!!!!!!!!!!!!!!!!!!! init_autocomplete %s' % __name__, file = sys.stderr)
     tagdb.init_autocomplete()
     app.run()
 
