222e0d8c2792af8af33bd814a96d6454e708bc5e
zyddnys <zyddnys@outlook.com>
2019-12-17 15:52:39 -0500

    improve autotag performance

--- services/autotag.py
+++ services/autotag.py
@@ -3,6 +3,7 @@ from db import tagdb as db
 from db import client
 from db.TagDB_language import translateTagToPreferredLanguage
 from utils.dbtools import MongoTransaction
+from collections import defaultdict
 
 import itertools
 
@@ -14,8 +15,6 @@ def _clearOrCreateCollections(session) :
 def buildTagRulesFromScratch(utag_threshold = 5, freq_threshold = 3, rule_threshold = 0.8) :
 	with MongoTransaction(client) as s :
 		_clearOrCreateCollections(s())
-		all_tags = db.db.tags.distinct('id', session = s())
-		all_utags = db.db.items.distinct('item.utags', session = s())
 		ret = db.db.items.aggregate([
 			{'$unwind': {'path': '$item.utags'}},
 			{'$group': {'_id': '$item.utags', 'count': {'$sum': 1}}}
@@ -26,23 +25,23 @@ def buildTagRulesFromScratch(utag_threshold = 5, freq_threshold = 3, rule_thresh
 
 	with MongoTransaction(client) as s :
 		in_mem_utag_freq = dict([(it['_id'], it['count']) for it in db.db.utag_freq.find({}, session = s())])
-		#in_mem_tag_utag_freq = {}
-		for (tag, utag) in itertools.product(all_tags, all_utags) :
-			if in_mem_utag_freq[utag] < utag_threshold :
-				continue
-			freq = db.db.items.count_documents({'$and': [{'tags': {'$in': [tag]}}, {'item.utags': {'$in': [utag]}}]}, session = s())
+		in_mem_tag_utag_freq = defaultdict(int)
+		for video_item in db.db.items.find({}, session = s()) :
+			cur_tags = video_item['tags']
+			cur_utags = video_item['item']['utags'] if 'utags' in video_item['item'] else []
+			for (tag, utag) in itertools.product(cur_tags, cur_utags) :
+				in_mem_tag_utag_freq[(tag, utag)] += 1
+		db.db.utag_tag_freq.insert_many([{'tag': tag, 'utag': utag, 'freq': freq} for ((tag, utag), freq) in in_mem_tag_utag_freq.items()], session = s())
+		for ((tag, utag), freq) in in_mem_tag_utag_freq.items() :
 			if freq < freq_threshold :
 				continue
-			db.db.utag_tag_freq.insert_one({'tag': tag, 'utag': utag, 'freq': freq}, session = s())
-			#in_mem_tag_utag_freq[f'{tag} {utag}'] = freq
+			if in_mem_utag_freq[utag] < utag_threshold :
+				continue
 			prob = float(freq) / float(in_mem_utag_freq[utag])
 			if prob > rule_threshold :
 				db.db.utag_rules.insert_one({'utag': utag, 'tag': tag}, session = s())
 				tag_obj = db.db.tags.find_one({'id': tag}, session = s())
 				print('Adding rule {%s} => {%s} with prob = %.2f%%' % (utag, translateTagToPreferredLanguage(tag_obj, 'CHS'), prob * 100))
-			else :
-				pass
-				#print('Ignoring rule {%s} => {%s} with prob = %.2f%%' % (utag, tag, prob * 100))
 		s.mark_succeed()
 
 def inferTagidsFromUtags(utags) :
