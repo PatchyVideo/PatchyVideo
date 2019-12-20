9fed4d8a1110ce686802f6cc76549092283b0704
zyddnys <zyddnys@outlook.com>
2019-12-20 10:19:44 -0500

    add log

--- ajax_backend/ajax_fetch_video_frontend.py
+++ ajax_backend/ajax_fetch_video_frontend.py
@@ -7,6 +7,7 @@ from flask import render_template, request, current_app, jsonify, redirect, sess
 from init import app
 from utils.interceptors import loginOptional, jsonRequest, loginRequiredJSON
 from utils.jsontools import *
+from utils.logger import log
 
 from spiders import dispatch
 from spiders.Twitter import Twitter
@@ -15,11 +16,14 @@ from spiders.Twitter import Twitter
 @loginRequiredJSON
 @jsonRequest
 def ajax_helper_get_twitter_info(rd, user, data):
+	log(obj = {'url': data.url})
 	obj, cleanURL = dispatch(data.url)
 	if obj.NAME != 'twitter' :
+		log(obj = {'msg': 'NOT_TWITTER'})
 		return makeResponseFailed('NOT_TWITTER')
 	info = obj.get_metadata(obj, cleanURL)
 	if info["status"] != 'SUCCEED' :
+		log(obj = {'msg': 'FETCH_FAILED', 'info': info})
 		return makeResponseFailed('FETCH_FAILED')
 	return info
 
@@ -27,10 +31,13 @@ def ajax_helper_get_twitter_info(rd, user, data):
 @loginRequiredJSON
 @jsonRequest
 def ajax_helper_get_ytb_info(rd, user, data):
+	log(obj = {'url': data.url})
 	obj, cleanURL = dispatch(data.url)
 	if obj.NAME != 'youtube' :
+		log(obj = {'msg': 'NOT_YOUTUBE'})
 		return makeResponseFailed('NOT_YOUTUBE')
 	info = obj.get_metadata(obj, cleanURL)
 	if info["status"] != 'SUCCEED' :
+		log(obj = {'msg': 'FETCH_FAILED', 'info': info})
 		return makeResponseFailed('FETCH_FAILED')
 	return info
