add4a03aba947f61f3f019db833ee51122295d35
zyddnys <zyddnys@outlook.com>
2019-12-24 10:11:21 -0500

    added get_video interface

--- ajax_backend/ajax_getvideo.py
+++ ajax_backend/ajax_getvideo.py
@@ -7,29 +7,40 @@ from flask import render_template, request, current_app, jsonify, redirect, sess
 from init import app
 from utils.interceptors import loginOptional, jsonRequest, loginRequiredJSON
 from utils.jsontools import *
+from utils.exceptions import UserError
 
 from spiders import dispatch
 
-from services.getVideo import getVideoDetail
+from services.getVideo import getVideoDetail, getVideoDetailWithTags
 from services.playlist import listPlaylistsForVideo
 from config import TagsConfig, VideoConfig
 
-"""
+
 @app.route('/getvideo.do', methods = ['POST'])
 @loginOptional
 @jsonRequest
 def ajax_getvideo(rd, user, data):
-	obj, tags = getVideoDetailWithTagObjects(data.vid)
+	vidid = data.vid
+	try:
+		obj, tags, category_tag_map, tag_category_map = getVideoDetailWithTags(vidid, 'CHS')
+	except UserError:
+		abort(404)
+
+	tag_by_category = category_tag_map
+	for category in tag_by_category :
+		tag_by_category[category] = list(sorted(tag_by_category[category]))
 	copies = []
 	for item in obj['item']['copies'] :
 		ver = getVideoDetail(item)
 		assert ver
 		copies.append(ver)
-	playlists = listPlaylistsForVideo(data.vid)
+	playlists = listPlaylistsForVideo(vidid)
+
 	return "json", makeResponseSuccess({
 		"video" : obj,
 		"tags" : tags,
 		"copies" : copies,
-		"playlists" : playlists
+		"playlists" : playlists,
+		"tag_by_category": tag_by_category
 	})
-"""
+
