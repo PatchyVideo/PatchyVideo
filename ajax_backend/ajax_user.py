3901f290bc97bc5a9229db97f967c865b882fa3e
zyddnys <zyddnys@outlook.com>
2019-12-29 18:09:51 -0500

    add ajax interface for setting video clearence and user access control

--- ajax_backend/ajax_user.py
+++ ajax_backend/ajax_user.py
@@ -47,3 +47,40 @@ def ajax_user_changedesc(rd, user, data):
 def ajax_user_changepass(rd, user, data):
 	update_password(user['_id'], data.old_pass, data.new_pass)
 
+@app.route('/user/admin/updaterole.do', methods = ['POST'])
+@loginRequiredJSON
+@jsonRequest
+def ajax_user_updaterole(rd, user, data):
+	updateUserRole(user['_id'], data.role, user)
+
+@app.route('/user/admin/updatemode.do', methods = ['POST'])
+@loginRequiredJSON
+@jsonRequest
+def ajax_user_updatemode(rd, user, data):
+	updateUserAccessMode(user['_id'], data.mode, user)
+
+@app.route('/user/admin/get_allowedops.do', methods = ['POST'])
+@loginRequiredJSON
+@jsonRequest
+def ajax_user_get_allowedops(rd, user, data):
+	ret = getUserAllowedOps(user['_id'], user)
+	return "json", makeResponseSuccess(ret)
+
+@app.route('/user/admin/get_deniedops.do', methods = ['POST'])
+@loginRequiredJSON
+@jsonRequest
+def ajax_user_get_get_deniedops(rd, user, data):
+	ret = getUserDeniedOps(user['_id'], user)
+	return "json", makeResponseSuccess(ret)
+
+@app.route('/user/admin/update_allowedops.do', methods = ['POST'])
+@loginRequiredJSON
+@jsonRequest
+def ajax_user_update_allowedops(rd, user, data):
+	updateUserAllowedOps(user['_id'], data.ops, user)
+
+@app.route('/user/admin/update_deniedops.do', methods = ['POST'])
+@loginRequiredJSON
+@jsonRequest
+def ajax_user_update_deniedops(rd, user, data):
+	updateUserDeniedOps(user['_id'], data.ops, user)
