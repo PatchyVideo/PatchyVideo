da13ad2ff9a22a702ec6f2babcd70999f2063fe1
unknown <zyddnys>
2019-12-30 05:27:20 +0800

    patchyvideo crawler bugfix

--- scraper/video/Patchyvideo.py
+++ scraper/video/Patchyvideo.py
@@ -1,37 +1,37 @@
 
-from . import Spider
+from . import Crawler
 from utils.jsontools import *
 from utils.encodings import makeUTF8
 from utils.html import getInnerText
 
-from services.getVideo import getVideoDetail
+from services.getVideo import getVideoDetailNoFilter
 
-class Patchyvideo( Spider ) :
+class Patchyvideo( Crawler ) :
 	NAME = 'patchyvideo'
 	PATTERN = r'^(https:\/\/|http:\/\/)?(www\.)?(patchyvideo\.com|127\.0\.0\.1:5000|localhost:5000)\/video\?id=\w+'
 	SHORT_PATTERN = r''
-	LOCAL_SPIDER = True
+	LOCAL_CRAWLER = True
 
 	def normalize_url( self, link ) :
 		return link
 
 	def unique_id( self, link ) :
 		vidid = link[link.rfind("=") + 1:]
-		vidobj = getVideoDetail(vidid)
+		vidobj = getVideoDetailNoFilter(vidid)
 		if vidobj is None :
 			return ''
 		return vidobj['item']['unique_id']
 
 	def get_metadata( self, link ) :
 		vidid = link[link.rfind("=") + 1:]
-		vidobj = getVideoDetail(vidid)
+		vidobj = getVideoDetailNoFilter(vidid)
 		if vidobj is None :
 			return makeResponseFailed({})
 		return makeResponseSuccess(vidobj['item'])
 		
 	async def get_metadata_async( self, link ) :
 		vidid = link[link.rfind("=") + 1:]
-		vidobj = getVideoDetail(vidid)
+		vidobj = getVideoDetailNoFilter(vidid)
 		if vidobj is None :
 			return makeResponseFailed({})
 		return makeResponseSuccess(vidobj['item'])
