ca69ab23a3683917dd8e26d88b9f02974d213596
zyddnys <zyddnys@localhost.localdomain>
2019-12-30 22:14:50 -0500

    update environment

--- run_worker_local.sh
+++ run_worker_local.sh
@@ -1,6 +1,6 @@
 export FLASK_ENV=development
 export REDISTOGO_URL_WORKER=redis://127.0.0.1:6379
-export IMAGE_PATH=../images
+export IMAGE_PATH=/home/zyddnys/pvdata/images
 export MONGODB_URL=mongodb://127.0.0.1:27017/patchyvideo
 export REDISTOGO_URL=127.0.0.1
 export ENABLE_TRANSACTION=false
