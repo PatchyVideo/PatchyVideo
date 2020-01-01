98386c289f7f041543f4ea48e2dde272511885a0
zyddnys <zyddnys@outlook.com>
2020-01-01 16:23:04 -0500

    (untested) youtube playlist crawler

--- scraper/playlist/Youtube.py
+++ scraper/playlist/Youtube.py
@@ -1,32 +1,60 @@
 
 import aiohttp
 from urllib.parse import urlparse, parse_qs
+from bson.json_util import loads
+
 from services.config import Config
+from utils.logger import log_ne
 
 class Youtube() :
-    def __init__(self) :
-        pass
+	def __init__(self) :
+		pass
 
-    def test(self, url) :
-        ret = urlparse(url)
-        querys = parse_qs(ret.query)
-        if 'youtube.com' in ret.netloc :
-            if 'list' in querys :
-                return True
-        return False
+	def test(self, url) :
+		ret = urlparse(url)
+		querys = parse_qs(ret.query)
+		if 'youtube.com' in ret.netloc :
+			if 'list' in querys :
+				return True
+		return False
 
-    def normalize_url(self, url) :
-        ret = urlparse(url)
-        querys = parse_qs(ret.query)
-        pid = querys['list'][0]
-        return f"https://www.youtube.com/playlist?list={pid}"
+	def normalize_url(self, url) :
+		ret = urlparse(url)
+		querys = parse_qs(ret.query)
+		pid = querys['list'][0]
+		return f"https://www.youtube.com/playlist?list={pid}"
 
-    def unique_id(self, url) :
-        ret = urlparse(url)
-        querys = parse_qs(ret.query)
-        pid = querys['list'][0]
-        return f"youtube-list:{pid}"
+	def get_pid(self, url) :
+		ret = urlparse(url)
+		querys = parse_qs(ret.query)
+		pid = querys['list'][0]
+		return pid
 
-    async def run(self, url) :
-        pass
+	async def run(self, url = None, website_pid = None) :
+		if url and not website_pid :
+			website_pid = self.get_pid(url)
+		nextPageToken = ''
+		async with aiohttp.ClientSession() as session:
+			while True :
+				for key in Config.YOUTUBE_API_KEYS.split(",") :
+					if nextPageToken :
+						api_url = f"https://www.googleapis.com/youtube/v3/playlistItems?pageToken={nextPageToken}&part=snippet&maxResults=50&playlistId={website_pid}&key={key}"
+					else :
+						api_url = f"https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId={website_pid}&key={key}"
+					async with session.get(api_url) as resp:
+						if resp.status == 200 :
+							apirespond = await resp.text()
+							break
+						else :
+							log_ne(op = 'youtube_playlist_run_async', level = 'WARN', obj = {'msg': 'FETCH_FAILED', 'key': key, 'resp': apirespond.content, 'url': api_url})
+				ret = loads(apirespond)
+				for item in ret['items'] :
+					video_id = item['resourceId']['videoId']
+					yield f"https://www.youtube.com/watch?v={video_id}"
+				if 'nextPageToken' in ret :
+					nextPageToken = ret['nextPageToken']
+					continue
+				else :
+					break
+
 
