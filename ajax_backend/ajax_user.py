8477fc85aa3dcef2e5d861d2f92edccb989f39de
zyddnys <zyddnys@outlook.com>
2020-02-05 00:02:51 -0500

    update user management

--- ajax_backend/ajax_user.py
+++ ajax_backend/ajax_user.py
@@ -116,39 +116,39 @@ def ajax_user_whoami(rd, user, data):
 @loginRequiredJSON
 @jsonRequest
 def ajax_user_updaterole(rd, user, data):
-	updateUserRole(user['_id'], data.role, user)
+	updateUserRole(data.uid, data.role, user)
 
 @app.route('/user/admin/updatemode.do', methods = ['POST'])
 @loginRequiredJSON
 @jsonRequest
 def ajax_user_updatemode(rd, user, data):
-	updateUserAccessMode(user['_id'], data.mode, user)
+	updateUserAccessMode(data.uid, data.mode, user)
 
 @app.route('/user/admin/get_allowedops.do', methods = ['POST'])
 @loginRequiredJSON
 @jsonRequest
 def ajax_user_get_allowedops(rd, user, data):
-	ret = getUserAllowedOps(user['_id'], user)
+	ret = getUserAllowedOps(data.uid, user)
 	return "json", makeResponseSuccess(ret)
 
 @app.route('/user/admin/get_deniedops.do', methods = ['POST'])
 @loginRequiredJSON
 @jsonRequest
 def ajax_user_get_get_deniedops(rd, user, data):
-	ret = getUserDeniedOps(user['_id'], user)
+	ret = getUserDeniedOps(data.uid, user)
 	return "json", makeResponseSuccess(ret)
 
 @app.route('/user/admin/update_allowedops.do', methods = ['POST'])
 @loginRequiredJSON
 @jsonRequest
 def ajax_user_update_allowedops(rd, user, data):
-	updateUserAllowedOps(user['_id'], data.ops, user)
+	updateUserAllowedOps(data.uid, data.ops, user)
 
 @app.route('/user/admin/update_deniedops.do', methods = ['POST'])
 @loginRequiredJSON
 @jsonRequest
 def ajax_user_update_deniedops(rd, user, data):
-	updateUserDeniedOps(user['_id'], data.ops, user)
+	updateUserDeniedOps(data.uid, data.ops, user)
 
 @app.route('/user/list_users.do', methods = ['POST'])
 @loginRequiredJSON
