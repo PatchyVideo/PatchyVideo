9c8137e384ed7d5d4d9c559322bfb922a4f72b00
zyddnys <zyddnys@outlook.com>
2020-02-18 11:17:55 -0500

    relate tag update

--- services/tagStatistics.py
+++ services/tagStatistics.py
@@ -12,54 +12,54 @@ from utils.logger import log
 from . import TAG_TRACKER_ADDRESS
 
 def getPopularTags(user_language, max_count = 20) :
-    try :
-        assert isinstance(max_count, int) and max_count <= 100 and max_count > 0
-        response = get_page(TAG_TRACKER_ADDRESS + "/get?count=%d" % max_count)
-        json_obj = json.loads(response)
-        tag_ids = [int(i) for i in json_obj['tags']]
-        return tagdb.translate_tag_ids_to_user_language(tag_ids, user_language)[0]
-    except :
-        return []
+	try :
+		assert isinstance(max_count, int) and max_count <= 100 and max_count > 0
+		response = get_page(TAG_TRACKER_ADDRESS + "/get?count=%d" % max_count)
+		json_obj = json.loads(response)
+		tag_ids = [int(i) for i in json_obj['tags']]
+		return tagdb.translate_tag_ids_to_user_language(tag_ids, user_language)[0]
+	except :
+		return []
 
 def getCommonTags(user_language, videos, max_count = 20) :
-    if len(videos) <= 0 :
-        return []
-    all_tags = list(itertools.chain(*[vid['tags'] for vid in videos]))
-    tag_map = Counter(all_tags).most_common(n = max_count)
-    tag_ids = [item[0] for item in tag_map]
-    return tagdb.translate_tag_ids_to_user_language(tag_ids, user_language)[0]
+	if len(videos) <= 0 :
+		return []
+	all_tags = list(itertools.chain(*[vid['tags'] for vid in videos]))
+	tag_map = Counter(all_tags).most_common(n = max_count)
+	tag_ids = [item[0] for item in tag_map]
+	return tagdb.translate_tag_ids_to_user_language(tag_ids, user_language)[0]
 
 def getCommonTagsWithCount(user_language, videos, max_count = 20) :
-    if len(videos) <= 0 :
-        return []
-    all_tags = list(itertools.chain(*[vid['tags'] for vid in videos]))
-    tag_map = Counter(all_tags).most_common(n = max_count)
-    tag_ids = [item[0] for item in tag_map]
-    return tagdb.translate_tag_ids_to_user_language_with_count(tag_ids, user_language)[0]
+	if len(videos) <= 0 :
+		return []
+	all_tags = list(itertools.chain(*[vid['tags'] for vid in videos]))
+	tag_map = Counter(all_tags).most_common(n = max_count)
+	tag_ids = [item[0] for item in tag_map]
+	return tagdb.translate_tag_ids_to_user_language_with_count(tag_ids, user_language)[0]
 
 @noexcept
 def updateTagSearch(tags) :
-    tag_ids = tagdb.filter_and_translate_tags(tags)
-    payload = {
-        'hitmap': dict.fromkeys(tag_ids, 1)
-    }
-    post_json(TAG_TRACKER_ADDRESS + "/hit", payload)
+	tag_ids = tagdb.filter_and_translate_tags(tags)
+	payload = {
+		'hitmap': dict.fromkeys(tag_ids, 1)
+	}
+	post_json(TAG_TRACKER_ADDRESS + "/hit", payload)
 
 def getRelatedTagsExperimental(user_language, tags, exclude = [], max_count = 10) :
-    log(obj = {'tags': tags, 'lang': user_language, 'count': max_count})
-    tag_ids = tagdb.filter_and_translate_tags(tags)
-    exclude_tag_ids = tagdb.filter_and_translate_tags(exclude)
-    exclude_tag_ids = list(set(exclude_tag_ids) - set(tag_ids))
-    related_items = tagdb.db.items.aggregate([
-        {'$match': {'$and': [{'tags': {'$all': tag_ids}}, {'tags': {'$nin': exclude_tag_ids}}]}},
-        {'$project': {'tags': 1}}
-    ])
-    total_counts = Counter(list(itertools.chain(*[item['tags'] for item in related_items])))
-    for tagid in tag_ids :
-        del total_counts[tagid]
-    total_count = sum(total_counts.values())
-    top_tags = total_counts.most_common(n = max_count)
-    top_tags_normalized = [(tagid, freq / total_count) for (tagid, freq) in top_tags]
-    tagid_to_tag_map = tagdb.translate_tag_ids_to_user_language_map([k for (k, v) in top_tags], user_language)
-    top_tags_translated = [{tagid_to_tag_map[tagid]: val} for (tagid, val) in top_tags_normalized]
-    return top_tags_translated
+	log(obj = {'tags': tags, 'lang': user_language, 'count': max_count})
+	tag_ids = tagdb.filter_and_translate_tags(tags)
+	exclude_tag_ids = tagdb.filter_and_translate_tags(exclude)
+	top_tags = list(tagdb.db.items.aggregate([
+		{'$match': {'tags': {'$all': tag_ids}}},
+		{'$project': {'tags': 1}},
+		{'$unwind': {'path': '$tags'}},
+		{'$group': {'_id': '$tags', 'count': {'$sum': 1}}},
+		{'$match': {'_id': {'$nin': exclude_tag_ids}}},
+		{'$sort': {'count': -1}},
+		{'$limit': max_count}
+		]))
+	total_count = sum(item['count'] for item in top_tags)
+	top_tags_normalized = [(item['_id'], item['count'] / total_count) for item in top_tags]
+	tagid_to_tag_map = tagdb.translate_tag_ids_to_user_language_map([item['_id'] for item in top_tags], user_language)
+	top_tags_translated = [{tagid_to_tag_map[tagid]: val} for (tagid, val) in top_tags_normalized]
+	return top_tags_translated
