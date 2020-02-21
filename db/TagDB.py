1c0ddda631d973d28c640277b27de92d197d28fc
zyddnys <zyddnys@outlook.com>
2020-02-21 02:23:33 -0500

    search bugfix

--- db/TagDB.py
+++ db/TagDB.py
@@ -203,6 +203,8 @@ class TagDB() :
 	def _translate_text_search(self, text) :
 		if '*' in text : # tag wildcard
 			return text
+		if text in ['site', 'date', 'placeholder', 'and', 'or', 'not', 'any', 'all', 'notag', 'true', 'false'] :
+			return text
 		serach_obj, serach_type = parse_search(text)
 		return ')' + dumps({'obj': serach_obj, 'type': serach_type})
 
