fe430f5907e307e5ca86d15c3aeac872f6d0f6b5
zyddnys <zyddnys@outlook.com>
2020-02-22 23:34:19 -0500

    avoid unnecessary requests

--- db/AutocompleteInterface.py
+++ db/AutocompleteInterface.py
@@ -25,30 +25,40 @@ class AutocompleteInterface() :
 		print('FAILED: %s message=%s' % (func, err_msg), file = sys.stderr)
 
 	def AddTag(self, list_of_tuple_of_tagid_count_category) :
+		if not list_of_tuple_of_tagid_count_category :
+			return
 		payload = "%d " % len(list_of_tuple_of_tagid_count_category)
 		for (tagid, count, category) in list_of_tuple_of_tagid_count_category :
 			payload += "%d %d %d " % (tagid, count, category)
 		self._post("AddTag", "addtag", payload)
 
 	def AddWord(self, list_of_tuple_of_tagid_word_lang) :
+		if not list_of_tuple_of_tagid_word_lang :
+			return
 		payload = "%d " % len(list_of_tuple_of_tagid_word_lang)
 		for (tagid, word, lang) in list_of_tuple_of_tagid_word_lang :
 			payload += "%d %s %s " % (tagid, word, lang)
 		self._post("AddWord", "addword", payload)
 
 	def SetCount(self, list_of_tuple_of_tagid_count) :
+		if not list_of_tuple_of_tagid_count :
+			return
 		payload = "%d " % len(list_of_tuple_of_tagid_count)
 		for (tagid, count) in list_of_tuple_of_tagid_count :
 			payload += "%d %d " % (tagid, count)
 		self._post("SetCount", "setcount", payload)
 
 	def SetCountDiff(self, list_of_tuple_of_tagid_diff) :
+		if not list_of_tuple_of_tagid_diff :
+			return
 		payload = "%d " % len(list_of_tuple_of_tagid_diff)
 		for (tagid, diff) in list_of_tuple_of_tagid_diff :
 			payload += "%d %d " % (tagid, diff)
 		self._post("SetCountDiff", "setcountdiff", payload)
 
 	def SetCat(self, list_of_tuple_of_tagid_cat) :
+		if not list_of_tuple_of_tagid_cat :
+			return
 		payload = "%d " % len(list_of_tuple_of_tagid_cat)
 		for (tagid, cat) in list_of_tuple_of_tagid_cat :
 			payload += "%d %d " % (tagid, cat)
