2f93196336455ee05ecdc34de549d4dfab681559
zyddnys <zyddnys@outlook.com>
2020-03-05 17:57:21 -0500

    update autotag

--- services/autotag.py
+++ services/autotag.py
@@ -14,6 +14,12 @@ def _clearOrCreateCollections(session) :
 	db.db.utag_tag_freq.delete_many({}, session = session)
 	db.db.utag_freq.delete_many({}, session = session)
 	db.db.utag_rules.delete_many({}, session = session)
+	db.db.utag_rules_2.delete_many({}, session = session)
+
+def purge() :
+	with MongoTransaction(client) as s :
+		_clearOrCreateCollections(s())
+		s.mark_succeed()
 
 def buildTagRulesFromScratch(vid_tags_threshold = 4, utag_threshold = 5, freq_threshold = 3, rule_threshold = 0.7) :
 	with MongoTransaction(client) as s :
@@ -75,8 +81,68 @@ def buildTagRulesFromTags() :
 					db.db.utag_rules.insert_one({'utag': w, 'tag': tag_obj['id']}, session = s())
 					print('Adding rule {%s} => {%s} with prob = %.2f%%' % (w, translateTagToPreferredLanguage(tag_obj, 'CHS'), 1 * 100))
 		
+def buildTagRulesFromScratchV2(utag_threshold = 5, freq_threshold = 3, rule_threshold = 0.75) :
+	purge()
+	with MongoTransactionDisabled(client) as s :
+		ret = db.db.items.aggregate([
+			{'$unwind': {'path': '$item.utags'}},
+			{'$group': {'_id': '$item.utags', 'count': {'$sum': 1}}}
+		], session = s())
+		for r in ret :
+			db.db.utag_freq.insert_one(r, session = s())
+		s.mark_succeed()
+
+	with MongoTransactionDisabled(client) as s :
+		in_mem_utag_freq = dict([(it['_id'], it['count']) for it in db.db.utag_freq.find({}, session = s())])
+		in_mem_tag_utag_freq = defaultdict(int)
+		for video_item in db.db.items.find({}, session = s()) :
+			cur_tags = list(filter(lambda x: x < 0x80000000, video_item['tags']))
+			cur_utags = video_item['item']['utags'] if 'utags' in video_item['item'] else []
+			for (tag, utag) in itertools.product(cur_tags, cur_utags) :
+				in_mem_tag_utag_freq[(tag, utag)] += 1
+		db.db.utag_tag_freq.insert_many([{'tag': tag, 'utag': utag, 'freq': freq} for ((tag, utag), freq) in in_mem_tag_utag_freq.items()], session = s())
+		for ((tag, utag), freq) in in_mem_tag_utag_freq.items() :
+			if freq < freq_threshold :
+				continue
+			if in_mem_utag_freq[utag] < utag_threshold :
+				continue
+			prob = float(freq) / float(in_mem_utag_freq[utag])
+			if prob > rule_threshold :
+				db.db.utag_rules.insert_one({'utag': utag, 'tag': tag}, session = s())
+				tag_obj = db.db.tags.find_one({'id': tag}, session = s())
+				print('Adding rule {%s} => {%s} with prob = %.2f%%' % (utag, translateTagToPreferredLanguage(tag_obj, 'CHS'), prob * 100))
+		s.mark_succeed()
+
+	with MongoTransactionDisabled(client) as s :
+		for tag_obj in db.db.tags.find() :
+			words = []
+			full_words = []
+			for (lang, value) in tag_obj['languages'].items() :
+				words += cut_for_search(value)
+				full_words.append(value)
+			for value in tag_obj['alias'] :
+				words += cut_for_search(value)
+				full_words.append(value)
+			words = list(set(words))
+			for w in full_words :
+				db.db.utag_rules.insert_one({'utag': w, 'tag': tag_obj['id']}, session = s())
+				print('Adding rule {%s} => {%s} with prob = %.2f%%' % (w, translateTagToPreferredLanguage(tag_obj, 'CHS'), 1 * 100))
+			for w1, w2 in itertools.combinations_with_replacement(words, 2) :
+				if w1 == w2 :
+					continue
+				db.db.utag_rules_2.insert_one({'utags': [w1, w2], 'tag': tag_obj['id']}, session = s())
+				print('Adding rule {%s,%s} => {%s} with prob = %.2f%%' % (w1, w2, translateTagToPreferredLanguage(tag_obj, 'CHS'), 1 * 100))
+
+	
+
 def inferTagidsFromUtags(utags) :
 	tags = [it['_id'] for it in db.db.utag_rules.aggregate([{'$match': {'utag': {'$in': utags}}}, {'$group':{'_id': '$tag'}}])]
+	for w1, w2 in itertools.combinations_with_replacement(utags, 2) :
+		if w1 == w2 :
+			continue
+		obj = db.db.utag_rules_2.find_one({'utags': {'$all': [w1, w2]}})
+		if obj :
+			tags.append(obj['tag'])
 	return list(set(tags))
 
 def inferTagsFromUtags(utags, user_language) :
