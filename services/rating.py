211d1da878d41a1758a32ed09d0fb16d139240d2
zyddnys <zyddnys@outlook.com>
2020-03-31 11:03:18 -0400

    rating bugfix

--- services/rating.py
+++ services/rating.py
@@ -22,7 +22,10 @@ def rateVideo(user, vid: ObjectId, stars: int) :
             user_rated = 0
             db.video_ratings.insert_one({'vid': vid, 'uid': ObjectId(user['_id']), 'v': int(stars)}, session = s())
         if 'total_rating' in video_obj :
-            db.items.update_one({'_id': vid}, {'$inc': {'total_rating': int(stars - rating_obj['v']), 'total_rating_user': int(1 - user_rated)}}, session = s())
+            if rating_obj :
+                db.items.update_one({'_id': vid}, {'$inc': {'total_rating': int(stars - rating_obj['v']), 'total_rating_user': int(1 - user_rated)}}, session = s())
+            else :
+                db.items.update_one({'_id': vid}, {'$inc': {'total_rating': int(stars), 'total_rating_user': int(1 - user_rated)}}, session = s())
         else :
             db.items.update_one({'_id': vid}, {'$set': {'total_rating': int(stars), 'total_rating_user': int(1)}}, session = s())
         s.mark_succeed()
@@ -37,12 +40,15 @@ def ratePlaylist(user, pid: ObjectId, stars: int) :
         user_rated = 0
         if rating_obj :
             user_rated = 1
-            db.playlist_ratings.update_one({'pid': pid, 'uid': ObjectId(user['_id'])}, {'$set': {'v': int(stars)}})
+            db.playlist_ratings.update_one({'pid': pid, 'uid': ObjectId(user['_id'])}, {'$set': {'v': int(stars)}}, session = s())
         else :
             user_rated = 0
-            db.playlist_ratings.insert_one({'pid': pid, 'uid': ObjectId(user['_id']), 'v': int(stars)})
+            db.playlist_ratings.insert_one({'pid': pid, 'uid': ObjectId(user['_id']), 'v': int(stars)}, session = s())
         if 'total_rating' in playlist_obj :
-            db.playlists.update_one({'_id': pid}, {'$inc': {'total_rating': int(stars - rating_obj['v']), 'total_rating_user': int(1 - user_rated)}}, session = s())
+            if rating_obj :
+                db.playlists.update_one({'_id': pid}, {'$inc': {'total_rating': int(stars - rating_obj['v']), 'total_rating_user': int(1 - user_rated)}}, session = s())
+            else :
+                db.playlists.update_one({'_id': pid}, {'$inc': {'total_rating': int(stars), 'total_rating_user': int(1 - user_rated)}}, session = s())
         else :
             db.playlists.update_one({'_id': pid}, {'$set': {'total_rating': int(stars), 'total_rating_user': int(1)}}, session = s())
         s.mark_succeed()
