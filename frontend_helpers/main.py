6262ead58efcb7e835caedd5e501ae5cac2cccf3
zyddnys <zyddnys@outlook.com>
2020-07-06 21:35:06 -0400

    add video stream

--- frontend_helpers/main.py
+++ frontend_helpers/main.py
@@ -3,4 +3,5 @@ from .init import app
 
 from . import postVideoHelpers
 from . import fileUploadHelpers
+from . import videoStreamHelper
 from . import proxy
---
+++ frontend_helpers/videoStreamHelper.py
@@ -0,0 +1,40 @@
+
+from .init import routes, init_funcs
+
+from scraper.video import dispatch
+
+from utils.jsontools import *
+from utils.logger import log
+from utils.interceptors import asyncJsonRequest
+
+from aiohttp import ClientSession
+import os
+
+if os.getenv("FLASK_ENV", "development") == "production" :
+   VIDEOSTREAM_ADDRESS = 'http://videostream:5006'
+else :
+    VIDEOSTREAM_ADDRESS = 'http://localhost:5006'
+
+async def dispatch_presite_extraction(info) :
+	if info['extractor'] == 'BiliBili' :
+		ret_info = {
+			'stream_url': info['url'],
+			'format': 'flv',
+			'filesize': info['filesize']
+		}
+		return makeResponseSuccess(ret_info)
+	else :
+		return makeResponseFailed('UNSUPPORTED_WEBSITE')
+
+@routes.post("/get_video_stream")
+@asyncJsonRequest
+async def get_video_stream_info(request):
+	rqjson = (await request.json())
+	url = rqjson['url']
+	async with ClientSession() as session:
+		async with session.post(VIDEOSTREAM_ADDRESS, json={'url': url}) as resp:
+			resp_json = await resp.json()
+	if 'vs_err' in resp_json :
+		return makeResponseFailed({"errinfo": resp_json['vs_err']})
+	else :
+		return await dispatch_presite_extraction(resp_json)
