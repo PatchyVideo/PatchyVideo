d96893912b5be4d7add1646959ebdfd469db3a95
zyddnys <zyddnys@outlook.com>
2021-01-21 15:27:52 -0500

    fix nicovideo mylist, now use nico's API

--- scraper/playlist/Nicovideo.py
+++ scraper/playlist/Nicovideo.py
@@ -12,9 +12,12 @@ from utils.logger import log_ne
 class Nicovideo() :
 	URL_MATCH = re.compile(r'mylist\/(\d+)')
 	EXTRACT_AVID = re.compile(r'(av\d+)')
-	DATA_MATCH_CONTENT = re.compile(r'Mylist\.preload\(\d+,(.+\}\])\);')
-	DATA_MATCH_META = re.compile(r'MylistGroup\.preloadSingle\(\d+,[\s]*({.+?})\);', re.S)
-	# TODO: known bug: content can not contain });
+	headers = {
+		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36',
+		'X-Frontend-Id': '6',
+		'X-Frontend-Version': '0',
+		'X-Niconico-Language': 'ja-jp'
+	}
 	def __init__(self) :
 		pass
 
@@ -35,17 +38,22 @@ class Nicovideo() :
 		return mylist_id
 
 	async def get_metadata(self, url = None) :
+		if not url :
+			return {
+				"desc": "not available",
+				"title": "not available"
+			}
+		website_pid = self.get_pid(self, url)
+		api_url = f"https://nvapi.nicovideo.jp/v2/mylists/{website_pid}?pageSize=1&page=1"
 		async with aiohttp.ClientSession() as session:
-			async with session.get(url) as resp:
+			async with session.get(api_url, headers = self.headers) as resp:
 				if resp.status == 200 :
 					web_content = await resp.text()
 				else :
+					web_content = await resp.text()
 					log_ne(op = 'nicovideo_mylist_get_metadata', level = 'WARN', obj = {'msg': 'FETCH_FAILED', 'url': url, 'resp': web_content.content})
 					raise Exception('failed to fetch playlist')
-		ret = self.DATA_MATCH_META.search(web_content)
-		meta_json_str, = ret.groups()
-		meta_json_str = meta_json_str.replace('\t', ' ')
-		meta_json = yaml.safe_load(meta_json_str)
+		meta_json = loads(web_content)['data']['mylist']
 		if not meta_json["description"] :
 			meta_json["description"] = f'Playlist from from {url}\nat {str(datetime.now())}'
 		return {
@@ -55,19 +63,19 @@ class Nicovideo() :
 
 	async def run(self, url = None, website_pid = None) :
 		if website_pid and not url :
-			url = f"https://www.nicovideo.jp/mylist/{website_pid}"
+			url = f"https://nvapi.nicovideo.jp/v2/mylists/{website_pid}?pageSize=1000&page=1"
+		
 		async with aiohttp.ClientSession() as session:
-			async with session.get(url) as resp:
+			async with session.get(url, headers = self.headers) as resp:
 				if resp.status == 200 :
 					web_content = await resp.text()
 				else :
+					web_content = await resp.text()
 					log_ne(op = 'nicovideo_mylist_run', level = 'WARN', obj = {'msg': 'FETCH_FAILED', 'url': url, 'resp': web_content.content})
 					raise Exception('failed to fetch playlist')
-		ret = self.DATA_MATCH_CONTENT.search(web_content)
-		data_json_str, = ret.groups()
-		data_json = loads(data_json_str)
+		data_json = loads(web_content)
 		
-		for item in data_json :
-			yield item['item_data']['video_id']
+		for item in data_json['data']['mylist']['items'] :
+			yield item['watchId']
 
 
