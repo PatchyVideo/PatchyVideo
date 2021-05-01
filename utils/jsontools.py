42023625cafcd6cf7aa5149b8e8ea35a20c4f982
zyddnys <zyddnys@outlook.com>
2021-05-01 19:04:13 -0400

    upgrade error handling

--- utils/jsontools.py
+++ utils/jsontools.py
@@ -4,28 +4,40 @@ from flask import current_app
 from bson.json_util import dumps, loads
 
 def makeResponseSuccess(data):
-    return {
-        "status": "SUCCEED",
-        "data": data
-    }
+	return {
+		"status": "SUCCEED",
+		"data": data
+	}
 
 def makeResponseFailed(data):
-    return {
-        "status": "FAILED",
-        "data": data
-    }
+	if isinstance(data, str) :
+		return {
+			"status": "FAILED",
+			"dataerr": {"reason": data}
+		}
+	else :
+		return {
+			"status": "FAILED",
+			"dataerr": data
+		}
 
 def makeResponseError(data):
-    return {
-        "status": "ERROR",
-        "data": data
-    }
-
+	if isinstance(data, str) :
+		return {
+			"status": "ERROR",
+			"dataerr": {"reason": data}
+		}
+	else :
+		return {
+			"status": "ERROR",
+			"dataerr": data
+		}
+		
 def jsonResponse(json_obj_or_str) :
-    if isinstance(json_obj_or_str, str) :
-        return current_app.response_class(json_obj_or_str + '\n', mimetype = current_app.config['JSONIFY_MIMETYPE'])
-    else :
-        return current_app.response_class(dumps(json_obj_or_str) + '\n', mimetype = current_app.config['JSONIFY_MIMETYPE'])
+	if isinstance(json_obj_or_str, str) :
+		return current_app.response_class(json_obj_or_str + '\n', mimetype = current_app.config['JSONIFY_MIMETYPE'])
+	else :
+		return current_app.response_class(dumps(json_obj_or_str) + '\n', mimetype = current_app.config['JSONIFY_MIMETYPE'])
 
 makeObject = Namespace.create_from_dict
 
